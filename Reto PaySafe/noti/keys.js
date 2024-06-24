ca3b4d967ceef37a607c60ee71f08c459058757d
ExterminadorD7 <EXTERMINADOR_D7@HOTMAIL.COM>
2024-06-24 10:49:29 -0500

    Add files via upload

---
+++ Reto PaySafe/noti/keys.js
@@ -0,0 +1,4 @@
+export default {
+    public: "pk_test_51PVEJJJAv1ZPs5WoB6A5uuwYvR8DOA9xGllm0gJjRuCxc7btx7lgqQftqzh9QvWsDdP2Q0iNkVOqqc7m23NrtDI600MB6OmQyq",
+    secret: "sk_test_51PVEJJJAv1ZPs5Wo16JACYq3B4CWw8qSaBtnS5E7gBFVkdxCzH4aqd6th8DotvBe45zhuKMlFAdWE7H5LNZ5FSYu0009OLSRg5"
+  }
